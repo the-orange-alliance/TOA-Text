675911b3099801647c4b50b6706860e4a88d41b7
Huppdo <dhupp2020@gmail.com>
2019-02-20 11:58:03 -0500

    Fix URL Later

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ default_app = firebase_admin.initialize_app(cred, {
 })
 
 app = Flask(__name__)
-apiURL = "0.0.0.0:80/"
+apiURL = "http://theorangealliance.org/api/"
 
 apiHeaders = {'content-type': 'application/json',
               'X-TOA-KEY': '',
